15d4193547edc87d587e210840eb38f4e2841be1
Delgersaikhan <delgersaikhan2001@gmail.com>
2020-01-07 13:14:10 +0800

    vuejs init and fetch sample

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-console.log("mainjs");
-var requestOptions = {
-  method: "GET",
-  redirect: "follow"
-};
-
-fetch(
-  "http://data.num.edu.mn/dataset/8679868d-004a-4a25-b4c1-0a9d36ba4945/resource/838e4aa1-b0fa-40c1-a7e4-71b9cb47fcfb/download/-2018-autumn.json",
-  requestOptions
-)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log("error", error));
+new Vue({
+  el: ".container",
+  data: {
+    msg: "my message",
+    class: {},
+    surguuli: "",
+    tenhim: "",
+    hicheel: ""
+  },
+  created() {
+    getClass();
+  },
+  methods: {
+    jishe() {},
+    getClass() {
+      var requestOptions = {
+        method: "GET",
+        redirect: "follow"
+      };
+      let data;
+      fetch(
+        "http://data.num.edu.mn/dataset/8679868d-004a-4a25-b4c1-0a9d36ba4945/resource/838e4aa1-b0fa-40c1-a7e4-71b9cb47fcfb/download/-2018-autumn.json",
+        requestOptions
+      )
+        .then(response => response.text())
+        .then(result => (data = result))
+        .catch(error => console.log("error", error));
+    }
+  }
+});
