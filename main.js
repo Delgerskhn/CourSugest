88c1758c42d08c5bd5e0dd1768f962f2b40b2cf2
KhenBai <56099382+KhenBai@users.noreply.github.com>
2020-01-08 16:08:33 +0800

    GetBagshClass

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ new Vue({
     isLoad: false
   },
   created() {
-    getClass();
-    getDun();
-    getBagsh();
+    // this.getClass();
+    // getDun();
+    this.getBagsh();
   },
   methods: {
-    jishe() {},
+    jishe() {
+      console.log(bagsh);
+    },
     getClass() {
       //tsetsneegiin select ugugduluu beldeh heseg
       var requestOptions = {
@@ -28,7 +30,7 @@ new Vue({
         "http://data.num.edu.mn/dataset/8679868d-004a-4a25-b4c1-0a9d36ba4945/resource/838e4aa1-b0fa-40c1-a7e4-71b9cb47fcfb/download/-2018-autumn.json",
         requestOptions
       )
-        .then(response => response.text())
+        .then(response => response.json())
         .then(result => (data = result))
         .catch(error => console.log("error", error));
     },
@@ -40,6 +42,22 @@ new Vue({
       //tuhain hichel derh bagsh nariin sanaliin indexiig return hiine
     },
     getDun() {},
-    getClass() {}
+    getBagsh() {
+      var requestOptions = {
+        method: "GET",
+        redirect: "follow"
+      };
+      let data;
+      fetch(
+        " http://data.num.edu.mn/dataset/787c396b-9ccc-4687-bd3a-71c345bc7fe3/resource/9901ef64-5539-4e05-9b70-1feffaecd615/download/-2018-autumn.json",
+        requestOptions
+      )
+        .then(response => response.json())
+        .then(result => {
+          bagsh = result;
+          console.log(bagsh);
+        })
+        .catch(error => console.log("error", error));
+    }
   }
 });
