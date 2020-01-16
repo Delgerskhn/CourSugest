8199762523d3caa4f71c4350b97704b9637025e5
KhenBai <56099382+KhenBai@users.noreply.github.com>
2020-01-16 18:59:31 +0800

    aasd

--- a.js
+++ a.js
@@ -1,1 +1,24 @@
-console.log("ahgdhagdahsd");
+let http = require("http");
+var fs = require("fs");
+http
+  .createServer(function(req, res) {
+    if (req.url == "/") a = "/client/src/views/index.html";
+    else a = req.url;
+    fs.readFile(__dirname + a, function(err, data) {
+      switch (req.url.split(".")[1]) {
+        case "css":
+          obj = { "Content-Type": "text/css" };
+          break;
+        case "js":
+          obj = { "Content-Type": "text/javascript" };
+          break;
+        default:
+          obj = { "Content-Type": "text/html" };
+      }
+      console.log(obj);
+      res.writeHead(200, obj);
+      res.write(data);
+      res.end();
+    });
+  })
+  .listen(8080);
