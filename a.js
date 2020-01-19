c4c4a69113840e67e7fc3a8ea6a696447f1a9445
D <deegiideegii00@gmail.com>
2020-01-19 11:47:50 +0700

    1.19

--- a.js
+++ a.js
@@ -1,1 +1,16 @@
-console.log("ahgdhagdahsd");
+var http = require('http');
+var fs = require('fs');
+var Path = require('Path')
+http.createServer(function(req,res) {
+
+    if(req.url === "/"){
+    fs.readFile(__dirname+'/client/src/views/index.html', function(err, data){
+     res.writeHead(200, {'Content-Type':'Text/html' });
+     res.write(data);
+     res.end();
+    });
+}
+   else if(req.url.match("\.css")){
+       var cssPath
+   }
+}).listen(5000,function(){console.log('server running on port 5000')});
