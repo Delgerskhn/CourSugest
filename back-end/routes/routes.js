27d73dac00028a1d179e1f857727dc1d6a29db29
KhenBai <56099382+KhenBai@users.noreply.github.com>
2020-01-19 19:00:03 +0800

    back end not finished

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -12,16 +12,52 @@ let appRouter = function(app) {
   });
   app.get("/classinfo/:id", (req, res) => {
     let classId = req.params.id;
+    console.log(classId);
+    let arr = new Array();
     fs.readFile(__dirname + "/hicheel.json", (err, data) => {
       if (err) throw err;
-      res.send(filterClass(data, classId));
+      arr = filterClass(data, classId);
+      console.log(arr);
+      classId = arr[0].Name;
+      fs.readFile(__dirname + "/teacher.json", (err, data) => {
+        if (err) throw err;
+        arr.push(filterTeacher(data, classId));
+        res.send(arr);
+      });
+    });
+  });
+  app.get("/test", (req, res) => {
+    fs.readFile(__dirname + "/teacher.json", (err, data) => {
+      if (err) throw err;
+      res.send(Filtering(data));
     });
   });
 };
-
-let filterClass = (data, clasId) => {
+let Filtering = data => {
   data = JSON.parse(data);
-  return data.filter(x => x.ID === clasId);
+  return data.map(function(x) {
+    return {
+      Name: x.Хичээлийн_нэр,
+      Index: x.Хичээлийн_индекс
+    };
+  });
+};
+
+let filterClass = (data, classId) => {
+  data = JSON.parse(data);
+  return data.filter(x => x.ID === classId);
+};
+
+let filterTeacher = (data, clasId) => {
+  data = JSON.parse(data);
+  data = data.filter(item => item.Хичээлийн_нэр === clasId);
+  let sum = data[0].Санал_өгсөн_суралцагчийн_тоо;
+  return data.map(function(x) {
+    return {
+      Name: x.Багшийн_нэр,
+      Index: x.Багшид_өгсөн_санал / sum
+    };
+  });
 };
 
 module.exports = appRouter;
