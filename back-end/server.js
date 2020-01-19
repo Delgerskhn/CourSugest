23221b80a266c9797dc1bf2d5cdad598246e301b
KhenBai <56099382+KhenBai@users.noreply.github.com>
2020-01-19 13:50:36 +0800

    new struct

---
+++ back-end/server.js
@@ -0,0 +1,14 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let routes = require("./routes/routes.js");
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+routes(app);
+
+let server = app.listen(8080, () => {
+  console.log("server started on port:", server.address().port);
+});
